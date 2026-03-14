60869c05266fef7c3652efce7453106193d245d4
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2026-03-14 23:22:54 +0500

    fix turbopack config

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"F1LSQQ1dlJDYXum0y29LDCAk9vZnHkUfqml7io6as1Q=\"\n}"
--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   reactStrictMode: true,
+  turbooaxk:{}
 };
 
 module.exports = withPWA(nextConfig);
